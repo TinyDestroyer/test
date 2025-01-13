6495dabb29d1db928509092b3d34f20de9525f55
TinyDestroyer <guptashubh.20208@gmail.com>
2025-01-14 02:35:52 +0530

    forth commit

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request){
+    console.log("yaha hote hai msgs");
     const { searchParams } = new URL(req.url);
     const query = searchParams.get("query");
     const msg = "hello Shubh!!! this is your query : " + query;
