cd9f167ad7d694014d3b4c494c219c136c17e06d
TinyDestroyer <guptashubh.20208@gmail.com>
2025-01-15 14:40:08 +0530

    chat bot integration

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,9 +1,26 @@
 import { NextResponse } from "next/server";
+import Groq from "groq-sdk";
+
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
 export async function GET(req: Request){
     console.log("yaha hote hai msgs");
+
     const { searchParams } = new URL(req.url);
     const query = searchParams.get("query");
-    const msg = "hello Shubh!!! this is your query : " + query;
-    return NextResponse.json(msg);
+    const msg = "hello Shubh!!! This is your query : " + query;
+    const data = await groq.chat.completions.create({
+        messages: [
+          {
+            role: "user",
+            content: "India",
+          },
+        ],
+        model: "llama-3.3-70b-versatile",
+        max_tokens: 300,
+    });
+
+    console.log("chalo jaa rha hu!!!");
+
+    return NextResponse.json(data.choices[0].message.content);
 }
